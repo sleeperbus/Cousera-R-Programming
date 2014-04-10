72a2b989990de31f14ae20b474b84e68eaa1b882
Hojoon Ji <sleeperbus@gmail.com>
2014-04-10 22:37:03 +0900

    Indent Change

--- week2/first_function.R
+++ week2/first_function.R
@@ -1,15 +1,15 @@
 add2 <- function(x, y) {
-  x + y
+    x + y
 }
 
 above10 <- function(x) {
-  use <- x > 10
-  x[use]
+    use <- x > 10
+    x[use]
 }
 
 above <- function(x, n = 10) {
-  use <- x > n
-  x[use]
+    use <- x > n
+    x[use]
 }
 
 
