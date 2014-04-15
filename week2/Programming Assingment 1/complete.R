afac9c188c710e2e95fdce621897945780b4969d
Hojoon Ji <sleeperbus@gmail.com>
2014-04-15 22:45:00 +0900

    2nd problem solved

--- week2/Programming Assingment 1/complete.R
+++ week2/Programming Assingment 1/complete.R
@@ -1,3 +1,13 @@
 complete <- function(directory, id = 1:332) {
+    files <- sprintf("%03d.csv", id)
+    files <- file.path(directory, files)
     
+    dataCount <- function(file) {
+        data <- read.csv(file)
+        count <- sum(complete.cases(data))
+        count
+    }
+    counts <- do.call("rbind", lapply(files, dataCount))
+    data <- data.frame(id, counts)
+    data
 }
