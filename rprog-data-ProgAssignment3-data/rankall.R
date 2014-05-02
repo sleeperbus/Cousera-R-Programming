da9666f09637df7c68e7b436de582122ecdc3671
Hojoon Ji <sleeperbus@gmail.com>
2014-05-02 23:46:46 +0900

    ...

--- rprog-data-ProgAssignment3-data/rankall.R
+++ rprog-data-ProgAssignment3-data/rankall.R
@@ -2,7 +2,6 @@ rankall <- function(outcome, num = "best") {
     ## Read outcome data
     
     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-#    data <- read.csv("outcome-of-care-measures.csv")
     data <- data[, c(2, 7, 11, 17, 23)]
     data[, 3] <- as.numeric(data[, 3])
     data[, 4] <- as.numeric(data[, 4])
@@ -16,22 +15,27 @@ rankall <- function(outcome, num = "best") {
     ## Check that outcome is valid
     if (!any(outcomes == outcome)) stop("invalid outcome") 
     
-    result <- do.call("rbind", lapply(states, function(s) {
-        r <- data[data$state == s, c("hospital", "state", outcome)]
-        r <- r[!is.na(r[, outcome]),]
-        r <- r[with(r, order(r[, outcome], r[, "hospital"])), ]  
-        
-        rows <- nrow(r)
-        if (num == "best") n <- 1
-        else if (num == "worst") n <- rows
-        else n <- num
-        
-        if (n > rows) result <- c(NA, s)
-        else result <- r[n, c("hospital", "state")]
-#        else result <- r[n, c("hospital", "state", outcome)]
-        result
-        
-    }));
+    result <- lapply(split(data, data$state), function(d) {
+        r <- d[, c("hospital", "state", outcome)]
+        r <- r[!is.na(r[, outcome]), ]
+        r <- r[with(r, order(r[, outcome], r[, "hospital"])), ]
+    })
+    
+#     result <- do.call("rbind", lapply(states, function(s) {
+#         r <- data[data$state == s, c("hospital", "state", outcome)]
+#         r <- r[!is.na(r[, outcome]),]
+#         r <- r[with(r, order(r[, outcome], r[, "hospital"])), ]  
+#         
+#         rows <- nrow(r)
+#         if (num == "best") n <- 1
+#         else if (num == "worst") n <- rows
+#         else n <- num
+#         
+#         if (n > rows) result <- c(NA, s)
+#         else result <- r[n, c("hospital", "state")]
+#         result
+#         
+#     }));
 
     return(result)
 }
