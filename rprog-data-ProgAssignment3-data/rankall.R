728c1b972d7cc007ad293a85b6a2de68eedc318e
Hojoon Ji <sleeperbus@gmail.com>
2014-05-02 16:37:38 +0900

    rankall done

--- rprog-data-ProgAssignment3-data/rankall.R
+++ rprog-data-ProgAssignment3-data/rankall.R
@@ -2,6 +2,7 @@ rankall <- function(outcome, num = "best") {
     ## Read outcome data
     
     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+#    data <- read.csv("outcome-of-care-measures.csv")
     data <- data[, c(2, 7, 11, 17, 23)]
     data[, 3] <- as.numeric(data[, 3])
     data[, 4] <- as.numeric(data[, 4])
@@ -10,16 +11,28 @@ rankall <- function(outcome, num = "best") {
                      "pneumonia")
     outcomes <- c("heart attack", "heart failure", "pneumonia")
     states <- unique(data$state)
+    states <- sort(states)
     
     ## Check that outcome is valid
     if (!any(outcomes == outcome)) stop("invalid outcome") 
     
     result <- do.call("rbind", lapply(states, function(s) {
-        result <- data[data$state == s, ]
-        result <- result[with(result, order(-result$outcome)), ]
+        r <- data[data$state == s, c("hospital", "state", outcome)]
+        r <- r[!is.na(r[, outcome]),]
+        r <- r[with(r, order(r[, outcome], r[, "hospital"])), ]  
         
-#        data.state <- data.state[!is.na(data.state[outcome]), ]
-        data.state <- data.state[num, c("hospital", "state", outcome)]
+        rows <- nrow(r)
+        if (num == "best") n <- 1
+        else if (num == "worst") n <- rows
+        else n <- num
+        
+        if (n > rows) result <- c(NA, s)
+        else result <- r[n, c("hospital", "state")]
+#        else result <- r[n, c("hospital", "state", outcome)]
+        result
+        
     }));
-    result <- result[with(result, order(state)), ]
+
+    return(result)
 }
+
